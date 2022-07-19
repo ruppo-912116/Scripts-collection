fc8d7dda8c5056c31406868fc08158f8ff7b2c5c
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2022-07-19 17:31:09 +0545

    articleLawHeader div accessed

--- open_gov_act/scraping.py
+++ open_gov_act/scraping.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 
 url = "https://wetten.overheid.nl/BWBR0045754/2022-05-01/0#Hoofdstuk5"
 
+my_dict = {}
+
 
 # get the html page
 htmlPage = requests.get(url).text
@@ -15,11 +17,11 @@ soup = BeautifulSoup(htmlPage,'html.parser')
 lawDivs = soup.find_all("div",class_="hoofdstuk") 
 
 for law in lawDivs:
-    # nestedSoup = law.find("div", class_="artikle")
-    nestedSoup = law.find_all("div", {"class": "artikle"})
-    print(law.prettify())
+    articleDiv = law.find("div", {"class": "artikel"})
+    articleLawHeaderDiv = articleDiv.div
+    print(articleLawHeaderDiv)
+    print('-----------------------------------------------------')
     break
-
 # [@id="HoofdstukI"]
 
 
